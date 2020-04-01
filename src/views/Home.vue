2cfc3d27007c1765422c614b82bbeea62fb5effc
limiu <845217637@qq.com>
2020-04-02 00:13:10 +0800

    add header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-   hello world
+   <home-header></home-header>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Heade'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
