1fa30185b3acced2684f336dc52888a82017937a
limiu <845217637@qq.com>
2020-04-05 22:31:53 +0800

    add search to city

--- src/views/components/city/City.vue
+++ src/views/components/city/City.vue
@@ -1,15 +1,19 @@
 <template>
-<city-header></city-header>
+<div>
+  <city-header></city-header>
+  <city-search></city-search>
+</div>
 </template>
 <script>
 import CityHeader from './compontents/Header'
+import CitySearch from './compontents/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
 <style lang="stylus" scoped>
-
 </style>
---
+++ src/views/components/city/compontents/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+  <input class="search-input" type="text" placeholder="搜索城市/行政区">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+  @import  '~@/assets/styles/varibles.styl'
+  .search
+    height .72rem
+    background $bgColor
+    padding 0 .1rem
+    .search-input
+      height .62rem
+      line-height .62rem
+      width 100%
+      text-align center
+      border-radius .06rem
+      color #666
+      padding 0 .1rem
+      box-sizing  border-box
+</style>
