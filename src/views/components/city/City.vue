f7d1f974df7b0d54db72801e04c2fc3954866317
limiu <845217637@qq.com>
2020-04-06 21:44:50 +0800

    finish city css

--- src/views/components/city/City.vue
+++ src/views/components/city/City.vue
@@ -2,16 +2,22 @@
 <div>
   <city-header></city-header>
   <city-search></city-search>
+  <city-list></city-list>
+  <city-alphabet></city-alphabet>
 </div>
 </template>
 <script>
 import CityHeader from './compontents/Header'
 import CitySearch from './compontents/Search'
+import CityList from './compontents/List'
+import CityAlphabet from './compontents/Alphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
