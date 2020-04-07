21db323c06c9784bd410a1e50c4117b56127bf53
limiu <845217637@qq.com>
2020-04-07 14:04:52 +0800

    search finish

--- src/views/components/city/compontents/Search.vue
+++ src/views/components/city/compontents/Search.vue
@@ -1,11 +1,61 @@
 <template>
+<div>
   <div class="search">
-  <input class="search-input" type="text" placeholder="搜索城市/行政区">
+  <input v-model="keyword" class="search-input" type="text" placeholder="搜索城市/行政区">
+  </div>
+  <div class="search-content" ref="search" v-show="keyword">
+    <ul>
+      <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+      <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+    </ul>
+  </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (const i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 ||
+            value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -23,4 +73,18 @@ export default {
       color #666
       padding 0 .1rem
       box-sizing  border-box
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      color #666
+      background  #fff
 </style>
